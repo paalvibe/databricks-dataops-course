e964939d233f183d887a655fe36428449f95e15a
Pål de Vibe <123729504+paalvibeknowit@users.noreply.github.com>
2024-03-27 15:40:13 +0100

    add catname fn

--- libs/catname/__init__.py
+++ libs/catname/__init__.py
@@ -1,32 +1,28 @@
-# must be passed in as param as databricks lib not available in UC cluster
-# from databricks.sdk.runtime import dbutils
 import inspect
-
-def username(dbutils):
-    # dbutils must be passed in as param as databricks lib not available in UC cluster
-    """Return username, stripped for dots, part of users's email to use as dev db prefix"""
-    email = dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get()
-    name = email.split("@")[0].replace(".", "").replace("-", "")
-    return name
+import re
 
 
-def catname_from_path(
-*, 
-tbl,
-db="nyc_workshop",
-catalog="training",
-env="dev",
-):
-    # Get dbutils from calling module, as databricks lib not available in UC cluster
-    dbutils = inspect.stack()[1][0].f_globals['dbutils']
-    if not tbl:
-        raise ValueError("tbl must be a non-empty string")
-    if not db:
-        raise ValueError("db must be a non-empty string")
-    db_prefix = ""
-    if env == "dev":
-        uname = username(dbutils)
-        db_prefix = f"dev_{uname}_"
-    return f"{catalog}.{db_prefix}{db}.{tbl}"
-    # ignore catalog for now, until UC is enabled
-    # return f"{db_prefix}{db}.{tbl}"
+def catname_from_path():
+    """Derive catalog name from repo data mesh structure, e.g.
+    /Repos/paal@foobar.foo/databricks-dataops-course/orgs/acme/domains/transport/projects/taxinyc/flows/prep/revenue/revenue
+    """
+    dbutils = inspect.stack()[1][0].f_globals["dbutils"]
+    nb_path = (
+        dbutils.notebook.entry_point.getDbutils()
+        .notebook()
+        .getContext()
+        .notebookPath()
+        .get()
+    )
+    print(f"nb_path: {nb_path}")
+    ret = re.search(
+        "\/Repos\/.+\/orgs\/([^/]+)\/domains\/([^/]+)\/projects\/([^/]+)\/flows\/([^/]+)\/.+",
+        nb_path,
+        re.IGNORECASE,
+    )
+    org = ret[1]
+    domain = ret[2]
+    project = ret[3]
+    # flow = ret[4]  # we discard flow for now
+    catalog = f"{org}_{domain}_{project}"
+    return catalog
