166632cfd50cfa9dce1a58b7049912eb92eea40d
paalvibe <paalvibe>
2024-04-10 11:36:46 +0000

    Get params

--- orgs/acme/domains/transport/projects/taxinyc/flows/prep/revenue/revenue_by_borough.py
+++ orgs/acme/domains/transport/projects/taxinyc/flows/prep/revenue/revenue_by_borough.py
@@ -10,6 +10,19 @@
 
 # COMMAND ----------
 
+def get_parameters():
+    all_args = dict(dbutils.notebook.entry_point.getCurrentBindings())
+    # remove '--' substring
+    all_args = {key.replace('--', ''): value for key, value in all_args.items()}
+    # parse values to correct format
+    all_args = {key: ast.literal_eval(value) for key, value in all_args.items()}
+    return all_args
+
+params = get_parameters()
+print("params: " + repr(params))
+
+# COMMAND ----------
+
 # context_str = dbutils.notebook.entry_point.getDbutils().notebook().getContext().toJson()
 ctx = dbutils.notebook.entry_point.getDbutils().notebook().getContext()
 nbpath = ctx.notebookPath().get()
