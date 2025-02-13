73a71b7c146f83b588d1bd7debb4ee3209362490
paalvibe <paalvibe>
2025-02-13 14:52:35 +0000

    Simple data contract WIP

---
+++ orgs/acme/domains/transport/projects/taxinyc/flows/prep/revenue_w_contract/deploy.py
@@ -0,0 +1,78 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC # Deploy jobs defined by deployment.yml
+
+# COMMAND ----------
+
+!pip install pyyaml
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ### Import libs
+
+# COMMAND ----------
+
+# Enable live reloading of libs, not needed now
+# %load_ext autoreload
+# %autoreload 2
+
+# COMMAND ----------
+
+import requests
+from libs.dataops.deploy.autojob import autojob
+from libs.dataops.job import run_job_by_name
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ### Create dev job
+
+# COMMAND ----------
+
+# Deploy jobs based on deployment.yml, in dev mode, specified by env param
+response = autojob(env="dev")
+
+# COMMAND ----------
+
+response
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ### Run job with python code
+
+# COMMAND ----------
+
+run_job_by_name(dbutils=dbutils, job_name=response['job_name'])
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ## Tasks for later
+# MAGIC ### Task: Deploy to prod
+
+# COMMAND ----------
+
+# os.environ['PIPELINE_ENV'] = 'prod'
+# Deploy jobs based on deployment.yml, in dev mode
+# prod_response = autojob(env="prod")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ## Task: Run prod job with python
+
+# COMMAND ----------
+
+# run_job_by_name(dbutils=dbutils, job_name=prod_response['job_name'])
+
+# COMMAND ----------
+
+
