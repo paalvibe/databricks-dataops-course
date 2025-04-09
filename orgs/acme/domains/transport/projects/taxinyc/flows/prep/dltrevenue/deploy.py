f118ee6f8fb83e8190c9afec8a78b7da0ecd002d
paalvibe <paalvibe>
2025-04-09 12:32:01 +0000

    New run fn

--- orgs/acme/domains/transport/projects/taxinyc/flows/prep/dltrevenue/deploy.py
+++ orgs/acme/domains/transport/projects/taxinyc/flows/prep/dltrevenue/deploy.py
@@ -5,7 +5,7 @@
 
 # COMMAND ----------
 
-!pip install git+https://github.com/brickops/brickops.git@6bf182678df9aa12ce0d24a858bc9eb1ec68c524
+!pip install git+https://github.com/brickops/brickops.git@ef74c32220b2e2d561e5150bcc380fd9766b740c
 
 # COMMAND ----------
 
@@ -52,6 +52,116 @@ spark.sql(f"CREATE DATABASE IF NOT EXISTS {db}")
 
 # COMMAND ----------
 
+from brickops.databricks.context import DbContext, current_env, get_context
+db_context = get_context()
+db_context
+
+# COMMAND ----------
+
+current_env(ctx)
+
+# COMMAND ----------
+
+from brickops.dataops.deploy.repo import git_source
+git_source(db_context)
+
+# COMMAND ----------
+
+from brickops.dataops.deploy.pipeline.buildconfig import build_pipeline_config
+from brickops.dataops.deploy.readconfig import read_config_yaml
+
+cfgyaml = "deployment.yml"
+env="dev"
+cfg = read_config_yaml(cfgyaml)
+cfg["git_source"] = git_source(db_context)
+pipeline_config = build_pipeline_config(
+        cfg=cfg,
+        env=env,
+        db_context=db_context,
+)
+
+# COMMAND ----------
+
+from brickops.databricks.context import DbContext
+from brickops.databricks.username import get_username
+from brickops.datamesh.naming import pipelinename
+from brickops.dataops.deploy.pipeline.buildconfig.enrichtasks import enrich_tasks
+from brickops.dataops.deploy.pipeline.buildconfig.pipeline_config import (
+    PipelineConfig,
+    defaultconfig,
+)
+from brickops.gitutils import clean_branch, commit_shortref
+
+
+# COMMAND ----------
+
+from typing import Any
+
+def depname(*, db_context: DbContext, env: str, git_src: dict[str, Any]) -> str:
+    """Compose deployment name from env and git config."""
+    if env == "prod":
+        return "prod"
+    uname = get_username(db_context)
+    branch = clean_branch(git_src["git_branch"])
+    short_ref = commit_shortref(git_src["git_commit"])
+    return f"{env}_{uname}_{branch}_{short_ref}"
+
+def build_context_parameters(env: str, tags: dict[str, Any]) -> list[dict[str, Any]]:
+    """Create a list of parameters containing the environment and git info."""
+    return [
+        {
+            "name": "pipeline_env",
+            "default": env,
+        },
+        {
+            "name": "git_url",
+            "default": tags["git_url"],
+        },
+        {
+            "name": "git_branch",
+            "default": tags["git_branch"],
+        },
+        {
+            "name": "git_commit",
+            "default": tags["git_commit"],
+        },
+    ]
+
+
+def _tags(*, cfg: dict[str, Any], depname: str, pipeline_env: str) -> dict[str, Any]:
+    return {
+        "deployment": depname,
+        "git_url": cfg["git_source"]["git_url"],
+        "git_branch": cfg["git_source"]["git_branch"],
+        "git_commit": cfg["git_source"]["git_commit"],
+        "pipeline_env": pipeline_env,
+    }
+
+
+# COMMAND ----------
+
+full_cfg = defaultconfig()
+full_cfg.update(cfg)
+full_cfg.name = pipelinename(db_context, env=env)
+dep_name = depname(db_context=db_context, env=env, git_src=full_cfg.git_source)
+tags = _tags(cfg=cfg, depname=dep_name, pipeline_env=env)
+full_cfg.tags = tags
+full_cfg.parameters.extend(build_context_parameters(env, tags))
+
+# COMMAND ----------
+
+full_cfg
+
+# COMMAND ----------
+
+full_cfg.schema
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
 # Deploy pipelines based on deployment.yml, in dev mode, specified by env param
 
 response = autopipeline(env="dev")
@@ -65,16 +175,15 @@ response
 
 # COMMAND ----------
 
-# For now we will not run pipeline by id, but name instead
-# as it survives a cluster reconnect, since name is idempotent
+# Run the pipeline by pipeline ID
 run_pipeline(
-    dbutils=dbutils, 
     pipeline_id=response["response"]["pipeline_id"]
 )
 
 # COMMAND ----------
 
-# Can be used when the pipeline created has the same name as one previously recreated
+# Can be used when the pipeline created has the same name as one previously recreated,
+# but note that names are no longer idempotent in Databricks
 # run_pipeline_by_name(dbutils=dbutils, 
 #    pipeline_name=response["pipeline_name"])
 
