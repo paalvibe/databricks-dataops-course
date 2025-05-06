a4eabd7029fee7624459cbc950462e5a8fd68048
Pål de Vibe <123729504+paalvibeknowit@users.noreply.github.com>
2025-05-06 14:31:18 +0200

    improve docs

--- course/02-Deploy-Tasks/03_study_output.py
+++ course/02-Deploy-Tasks/03_study_output.py
@@ -10,7 +10,7 @@
 # MAGIC
 # MAGIC ## Task: Find the output data in Catalog page (from the left menu bar).
 # MAGIC
-# MAGIC What are the full Unity Catalog names of the output datasets? It will be under the catalog `transport`.
+# MAGIC What are the full Unity Catalog names of the output datasets? It will be under the catalog `acme_transport_taxinyc`.
 
 # COMMAND ----------
 
